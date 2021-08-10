7151d9201158a311875aaffb4eb05c7d40f61c8f
gi8 <giottolino@gmail.com>
2021-08-10 15:33:06 +0200

    set version to 1.1.0

--- cmd/root.go
+++ cmd/root.go
@@ -11,7 +11,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var version string = "1.0.2"
+var version string = "1.1.0"
 
 var rootCmd = &cobra.Command{
 	Use:           "certinfo HOSTNAME [HOSTNAME ...]",

735303a5739c89629b6047b56c62b5931db7189a
gi8 <giottolino@gmail.com>
2022-02-20 14:23:15 +0100

    fix: version const

--- cmd/root.go
+++ cmd/root.go
@@ -11,7 +11,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-const version string = "1.3.4"
+const version = "1.3.5"
 
 var rootCmd = &cobra.Command{
 	Use:           "certinfo HOSTNAME [HOSTNAME ...]",
